218de01f42f212ec2fdf77f34d8673b31701ca26
Kanika Saini <kanika16047@iiitd.ac.in>
2018-07-19 22:29:23 +0530

    Add files via upload

---
+++ gymsoftware/src/application/Controller.java
@@ -0,0 +1,55 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+	@FXML protected void handler1(ActionEvent event) throws Exception {
+
+	}
+
+	@FXML protected void handler2(ActionEvent event) throws Exception {
+
+	}
+
+	@FXML protected void handler3(ActionEvent event) throws Exception {
+
+	}
+
+	@FXML protected void handler4(ActionEvent event) throws Exception {
+
+	}
+
+	@FXML protected void handler5(ActionEvent event) throws Exception {
+
+	}
+
+	@FXML protected void handler6(ActionEvent event) throws Exception {
+
+	}
+
+	@FXML protected void handler7(ActionEvent event) throws Exception {
+
+	}
+
+	@FXML protected void handler8(ActionEvent event) throws Exception {
+
+	}
+
+	@FXML protected void handler9(ActionEvent event) throws Exception {
+
+	}
+
+	@FXML protected void handler10(ActionEvent event) throws Exception {
+
+	}
+
+	@FXML protected void handler11(ActionEvent event) throws Exception {
+
+	}
+
+	@FXML protected void handler12(ActionEvent event) throws Exception {
+
+	}
+
+}
